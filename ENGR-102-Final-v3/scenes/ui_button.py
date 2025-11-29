84167dbb4d04e06297bd33a69f9f953cf208e064
Dnyanesh Ramade <69685320+Dnyanesh132@users.noreply.github.com>
2025-11-29 16:45:24 -0600

    Add files via upload

---
+++ ENGR-102-Final-v3/scenes/ui_button.py
@@ -0,0 +1,28 @@
+import pygame
+
+#button class
+#used generative ai
+
+class Button:
+    def __init__(self, text, pos, font, base_color, hover_color):
+        self.text = text
+        self.font = font
+        self.base_color = base_color
+        self.hover_color = hover_color
+
+        self.surface = self.font.render(text, True, base_color)
+        self.rect = self.surface.get_rect(center=pos)
+
+    def draw(self, screen):
+        screen.blit(self.surface, self.rect)
+
+    def update(self, mouse_pos):
+        if self.rect.collidepoint(mouse_pos):
+            self.surface = self.font.render(self.text, True, self.hover_color)
+            return True
+        else:
+            self.surface = self.font.render(self.text, True, self.base_color)
+            return False
+        
+    def clicked(self, mouse_pos, event):
+        return event.type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(mouse_pos)
